49147eaa117906f04faabbf776440a57715e89bf
Ben Alexander <ben250402@yahoo.co.uk>
2025-07-14 18:26:54 +0100

    2 Added IOData and Result

---
+++ IOData/Output/Result.cs
@@ -0,0 +1,33 @@
+﻿namespace IOData.Output
+{
+	/// <summary>
+	/// A generic result wrapper containing a status and an optional value
+	/// </summary>
+	/// <typeparam name="T">An <see cref="Enum"/> type representing the status of the result</typeparam>
+	/// <typeparam name="U">The type of the result value</typeparam>
+	public class Result<T, U> where T : Enum
+	{
+		/// <summary>
+		/// The status indicating the outcome of the operation
+		/// </summary>
+		public T Status { get; }
+
+		/// <summary>
+		/// Optional value associated with the result
+		/// </summary>
+		public U? Value { get; }
+
+
+		/// <summary>
+		/// Constructor for <see cref="Result{T, U}"/>.
+		/// </summary>
+		/// <param name="status">The status indicating the outcome of the operation</param>
+		/// <param name="value">The optional value associated with the result</param>
+		public Result(T status, U? value = default)
+		{
+			Status = status;
+			Value = value;
+		}
+	}
+
+}
