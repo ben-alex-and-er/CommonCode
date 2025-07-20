96297526b67aa2d80a71b0c3b000001f0df671c8
Ben Alexander <ben250402@yahoo.co.uk>
2025-07-20 10:19:56 +0100

    4 Added TransactionToolKit

---
+++ TransactionToolkit/TransactionCreator.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+
+namespace TransactionToolkit
+{
+	using Interfaces;
+
+
+	/// <summary>
+	/// Provider for creating transactions
+	/// </summary>
+	/// <typeparam name="T">Type of DbContext</typeparam>
+	public class TransactionCreator<T> : ITransactionCreator where T : DbContext
+	{
+		private readonly T context;
+
+
+		/// <summary>
+		/// Constructor for <see cref="TransactionCreator{T}"/>
+		/// </summary>
+		/// <param name="context"></param>
+		public TransactionCreator(T context)
+		{
+			this.context = context;
+		}
+
+
+		IDbContextTransaction ITransactionCreator.CreateTransaction()
+			=> context.Database.BeginTransaction();
+
+		Task<IDbContextTransaction> ITransactionCreator.CreateTransactionAsync()
+			=> context.Database.BeginTransactionAsync();
+	}
+}
