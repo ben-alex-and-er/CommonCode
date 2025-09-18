b434e481a5b25412f1350e828b705c14301b9253
Ben Alexander <ben250402@yahoo.co.uk>
2025-09-18 19:57:42 +0100

    7 Changed %%package to %%p

--- .github/scripts/push-packages.cmd
+++ .github/scripts/push-packages.cmd
@@ -1,6 +1,7 @@
 @echo off
 echo === Pushing packages to NuGet ===
-for %%package in (artifacts\*.nupkg) do (
-  echo Pushing %%package...
-  dotnet nuget push %%package --api-key %1 --source https://api.nuget.org/v3/index.json --skip-duplicate
+
+for %%p in (artifacts\*.nupkg) do (
+  echo Pushing %%p...
+  dotnet nuget push "%%p" --api-key %1 --source https://api.nuget.org/v3/index.json --skip-duplicate
 )
