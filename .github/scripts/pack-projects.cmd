f0172a1549dfe528cca1b8a10f70853c54d8778c
Ben Alexander <ben250402@yahoo.co.uk>
2025-09-18 20:02:49 +0100

    7 Removed unused scripts and steps

--- .github/scripts/pack-projects.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-echo === Packing all projects ===
-for %%project in (*.csproj) do (
-  echo Packing %%project...
-  dotnet pack %%project --configuration Release --no-build -o ./artifacts
-)
