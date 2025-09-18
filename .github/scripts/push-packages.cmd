bd91a9164e18e40a96264622f7bcd180638179aa
Ben Alexander <ben250402@yahoo.co.uk>
2025-09-18 20:02:29 +0100

    7 Added quotes for parsing

--- .github/scripts/push-packages.cmd
+++ .github/scripts/push-packages.cmd
@@ -3,5 +3,5 @@ echo === Pushing packages to NuGet ===
 
 for %%p in (artifacts\*.nupkg) do (
   echo Pushing %%p...
-  dotnet nuget push "%%p" --api-key %1 --source https://api.nuget.org/v3/index.json --skip-duplicate
+  dotnet nuget push "%%p" --api-key "%~1" --source https://api.nuget.org/v3/index.json --skip-duplicate
 )
