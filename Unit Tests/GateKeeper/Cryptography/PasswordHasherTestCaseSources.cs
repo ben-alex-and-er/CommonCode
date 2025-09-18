7a02c9a84f4dbe1e29735a84efcee75de61ef0d5
Ben Alexander <ben250402@yahoo.co.uk>
2025-09-18 09:32:16 +0100

    6 Added PasswordHasher UNIT Tests

---
+++ Unit Tests/GateKeeper/Cryptography/PasswordHasherTestCaseSources.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography;
+
+
+namespace Unit_Tests.GateKeeper.Cryptography
+{
+	internal class PasswordHasherTestCaseSources
+	{
+		public static IEnumerable<TestCaseData> HashPasswordTestCaseSource
+		{
+			get
+			{
+				yield return new TestCaseData(0, 3123, 1, HashAlgorithmName.SHA1);
+				yield return new TestCaseData(43, 3, 4321, HashAlgorithmName.SHA256);
+				yield return new TestCaseData(1, 1, 654, HashAlgorithmName.SHA3_256);
+				yield return new TestCaseData(324, 213, 05, HashAlgorithmName.SHA384);
+				yield return new TestCaseData(5325, 4312, 99999, HashAlgorithmName.SHA3_384);
+				yield return new TestCaseData(4234, 4, 123, HashAlgorithmName.SHA3_512);
+				yield return new TestCaseData(5, 532, 2, HashAlgorithmName.SHA512);
+			}
+		}
+
+		public static IEnumerable<TestCaseData> VerifyPasswordTestCaseSource
+		{
+			get
+			{
+				yield return new TestCaseData(string.Empty, 0, 3123, 1, HashAlgorithmName.SHA1);
+				yield return new TestCaseData("test", 43, 3, 4321, HashAlgorithmName.SHA256);
+				yield return new TestCaseData("reallyreallyreallyreallyreallyreallylongpassword", 1, 1, 654, HashAlgorithmName.SHA3_256);
+				yield return new TestCaseData("%^#&@*(#@&*#", 324, 213, 05, HashAlgorithmName.SHA384);
+				yield return new TestCaseData("12313621783", 5325, 4312, 99999, HashAlgorithmName.SHA3_384);
+				yield return new TestCaseData(",.,'[.;[123;1.", 4234, 4, 123, HashAlgorithmName.SHA3_512);
+				yield return new TestCaseData("password", 5, 532, 2, HashAlgorithmName.SHA512);
+			}
+		}
+	}
+}
---
+++ Unit Tests/GateKeeper/Cryptography/PaswordHasherTests.cs
@@ -0,0 +1,36 @@
+﻿using GateKeeper.Cryptography;
+using System.Security.Cryptography;
+
+
+namespace Unit_Tests.GateKeeper.Cryptography
+{
+	internal class PaswordHasherTests
+	{
+		[TestCaseSource(typeof(PasswordHasherTestCaseSources), nameof(PasswordHasherTestCaseSources.HashPasswordTestCaseSource))]
+		public void HashPasswordTests(int saltSize, int hashSize, int iterations, HashAlgorithmName algorithmName)
+		{
+			// Act
+			var result = PasswordHasher.HashPassword("test", algorithmName, saltSize, hashSize, iterations);
+
+			// Assert
+			Assert.That(result, Is.Not.Null);
+			Assert.That(Convert.FromBase64String(result.Salt).Length, Is.EqualTo(saltSize));
+			Assert.That(result.HashSize, Is.EqualTo(hashSize));
+			Assert.That(result.Iterations, Is.EqualTo(iterations));
+			Assert.That(result.Algorithm, Is.EqualTo(algorithmName));
+		}
+
+		[TestCaseSource(typeof(PasswordHasherTestCaseSources), nameof(PasswordHasherTestCaseSources.VerifyPasswordTestCaseSource))]
+		public void VerifyPasswordTests(string password, int saltSize, int hashSize, int iterations, HashAlgorithmName algorithmName)
+		{
+			// Arrange
+			var hashedPassword = PasswordHasher.HashPassword(password, algorithmName, saltSize, hashSize, iterations);
+
+			// Act
+			var result = PasswordHasher.VerifyPassword(password, hashedPassword);
+
+			// Assert
+			Assert.That(result);
+		}
+	}
+}
